44c1f40b94bf5a82c6c1af8a3b41a52b33f8af54
ruthmazango <ruthmazango@gmail.com>
2023-07-18 10:40:23 +0200

    added relevant dependencies to make it RESTful

--- src/main/java/com/system/recipeblog/controller/CategoryController.java
+++ src/main/java/com/system/recipeblog/controller/CategoryController.java
@@ -1,8 +1,8 @@
 package com.system.recipeblog.controller;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
-@Controller
+@RestController
 public class CategoryController {
 }
